ce728e3c872bb705e9ed15984c4bbd47979a6f58
Olga Zotova <olgazotova1976@yandex.ru>
2026-01-26 14:14:36 +0300

    The tasks are solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b * b - 4 * a * c;
+
+    if (d < 0) {
+      return arr;
+    }
+
+    if (d === 0) {
+      let x = -b / (2 * a);
+      arr.push(x);
+      return arr;
+    }
+
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+let monthlyPercent = (percent / 100) / 12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+    let totalPayment = monthlyPayment * countMonths;
+    let roundedTotalPayment = Number(totalPayment.toFixed(2));
+    return roundedTotalPayment;
   
 }
