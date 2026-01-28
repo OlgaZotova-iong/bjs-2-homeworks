0c159b21d714768d2af571acde5e56e8e04183d1
Olga Zotova <olgazotova1976@yandex.ru>
2026-01-28 15:18:56 +0300

    The third task is completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   const result = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, array) => {
+      acc += age;
+      if (index === array.length - 1) {
+        return acc / array.length;
+      }
+      return acc;
+    }, 0);
+
+  return result;
 }
